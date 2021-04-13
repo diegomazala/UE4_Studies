bfdacc1f7822c485ff584d77fa9ca1bd5dc87e02
DiegoMazala@AcuteArt <diegomazala@gmail.com>
2021-04-13 16:43:57 +0100

    ue4 licensing system with crypto test

--- Plugins/LicenseSystemPlugin/Source/LicenseSystemPlugin/Private/LicenseSystemPluginBPLibrary.cpp
+++ Plugins/LicenseSystemPlugin/Source/LicenseSystemPlugin/Private/LicenseSystemPluginBPLibrary.cpp
@@ -5,8 +5,8 @@
 #include "EncryptionContextOpenSSL.h"
 #include "Misc/FileHelper.h"
 #include "Misc/Paths.h"
-
 #include "Containers/UnrealString.h"
+
 
 ULicenseSystemPluginBPLibrary::ULicenseSystemPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
@@ -23,15 +23,12 @@ float ULicenseSystemPluginBPLibrary::LicenseSystemPluginSampleFunction(float Par
 
 bool ULicenseSystemPluginBPLibrary::LoadLicense(FString LicenseFilename, FString KeyFilename)
 {
-	//FString filePath = *FPaths::GamePluginsDir() + folder + "/" + name;
-	//FFileHelper::SaveStringToFile(SaveTextB, *(FPaths::GameDir() + FileNameB));
-
-	LicenseFilename = "c:/tmp/crypto/AcuteArt.aa";
-	KeyFilename = "c:/tmp/crypto/AcuteArt.key";
+	LicenseFilename = "c:/tmp/crypto/aes.enc";
+	KeyFilename = "c:/tmp/crypto/aes.key";
 	FString EncryptedFilename = "c:/tmp/crypto/UE4_AcuteArt.aa";
-	FString DecryptedFilename = "c:/tmp/crypto/UE4_AcuteArt.json";
+	FString DecryptedFilename = "c:/tmp/crypto/UE4_aes.json";
 
-	 TArray<uint8> License, Key;
+	TArray<uint8> License, Key;
 
 	if (FPaths::FileExists(LicenseFilename))
 	{
@@ -49,36 +46,31 @@ bool ULicenseSystemPluginBPLibrary::LoadLicense(FString LicenseFilename, FString
 		}
 	}
 
-	FString InputFilename = "c:/tmp/crypto/AcuteArt.json";
-	TArray<uint8> InputData;
-	if (!FFileHelper::LoadFileToArray(InputData, *InputFilename))
-	{
-		UE_LOG(LogTemp, Error, TEXT("Could not load key file: %s"), *InputFilename);
-	}
-
 	
-	TArray<uint8> Iv = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
-	//Iv.Append(Key.GetData(), 16);
+	TArray<uint8> Iv(Key.GetData(), 128 / 8);;
+	//TArray<uint8> Iv;
+	//Iv.SetNumZeroed(16);
+	
 
 	EPlatformCryptoResult OpenSslResult;
 	FEncryptionContextOpenSSL* OpenSslCtx = new FEncryptionContextOpenSSL();
 
 
 
-	TArray<uint8> Encrypted = OpenSslCtx->Encrypt_AES_256_CBC(InputData, Key, Iv, OpenSslResult);
+	//TArray<uint8> Encrypted = OpenSslCtx->Encrypt_AES_256_CBC(InputData, Key, Iv, OpenSslResult);
 
-	if (OpenSslResult == EPlatformCryptoResult::Success)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Encryption successfully"), *InputFilename);
-		FFileHelper::SaveArrayToFile(Encrypted, *EncryptedFilename);
-	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("Could not decrypt file: %s"), *InputFilename);
-	}
+	//if (OpenSslResult == EPlatformCryptoResult::Success)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Encryption successfully"), *InputFilename);
+	//	FFileHelper::SaveArrayToFile(Encrypted, *EncryptedFilename);
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("Could not decrypt file: %s"), *InputFilename);
+	//}
 
 
-	TArray<uint8> Decrypted = OpenSslCtx->Decrypt_AES_256_CBC(Encrypted, Key, Iv, OpenSslResult);
+	TArray<uint8> Decrypted = OpenSslCtx->Decrypt_AES_256_CBC(License, Key, Iv, OpenSslResult);
 	
 	if (OpenSslResult == EPlatformCryptoResult::Success)
 	{
